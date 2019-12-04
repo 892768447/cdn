31841d5155071677b10a74c9bc1fc93f11a62e3d
Irony <892768447@qq.com>
2019-12-04 19:58:15 +0800

    2019-12-04,19:12:1575460695

--- assets/admin.js
+++ assets/admin.js
@@ -653,4 +653,11 @@ module.exports=function(r){var t={};function e(n){if(t[n])return t[n].exports;va
 
 flarum.extensions['irony-bing-wallpaper']=module.exports;
 
+var module={};
+
+module.exports=function(t){var e={};function n(r){if(e[r])return e[r].exports;var o=e[r]={i:r,l:!1,exports:{}};return t[r].call(o.exports,o,o.exports,n),o.l=!0,o.exports}return n.m=t,n.c=e,n.d=function(t,e,r){n.o(t,e)||Object.defineProperty(t,e,{enumerable:!0,get:r})},n.r=function(t){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(t,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(t,"__esModule",{value:!0})},n.t=function(t,e){if(1&e&&(t=n(t)),8&e)return t;if(4&e&&"object"==typeof t&&t&&t.__esModule)return t;var r=Object.create(null);if(n.r(r),Object.defineProperty(r,"default",{enumerable:!0,value:t}),2&e&&"string"!=typeof t)for(var o in t)n.d(r,o,function(e){return t[e]}.bind(null,o));return r},n.n=function(t){var e=t&&t.__esModule?function(){return t.default}:function(){return t};return n.d(e,"a",e),e},n.o=function(t,e){return Object.prototype.hasOwnProperty.call(t,e)},n.p="",n(n.s=9)}([function(t,e){t.exports=flarum.core.compat.app},function(t,e,n){"use strict";function r(t,e){t.prototype=Object.create(e.prototype),t.prototype.constructor=t,t.__proto__=e}n.d(e,"a",(function(){return r}))},,,,,function(t,e){t.exports=flarum.core.compat["components/SettingsModal"]},,,function(t,e,n){"use strict";n.r(e);var r=n(0),o=n.n(r),i=n(1),u=n(6),a=function(t){function e(){return t.apply(this,arguments)||this}Object(i.a)(e,t);var n=e.prototype;return n.className=function(){return"Modal--small"},n.title=function(){return app.translator.trans("flarum-ext-code-insert.admin.title")},n.form=function(){return[m("div",{className:"Form-group"},m("label",null,app.translator.trans("flarum-ext-code-insert.admin.modes_label")),m("input",{required:!0,className:"FormControl",value:"python,c_cpp,css,javascript",bidi:this.setting("irony.code.insert.modes","python,c_cpp,css,javascript")}))]},e}(n.n(u).a);o.a.initializers.add("irony-code-insert",(function(t){t.extensionSettings["irony-code-insert"]=function(){return t.modal.show(new a)}}))}]);
+;
+
+flarum.extensions['irony-code-insert']=module.exports;
+
 //# sourceMappingURL=https://pyqt.site/assets/admin.js.map
